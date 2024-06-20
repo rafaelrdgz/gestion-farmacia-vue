6d97a585164fab2d16c8e7bbb03a98d75980f2e7
rafaelrdgz <rafaelrdgz2003@gmail.com>
2024-06-19 22:40:23 -0400

    agregados media-queries vista movil

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,21 @@ import { useField, useForm } from 'vee-validate'
 import Medicamento from '@/Medicamento.js'
 import { ref } from 'vue'
 
-
 let medicamentos = new Map()
 
 const { handleSubmit, handleReset } = useForm({
   validationSchema: {
-    nombre (value) {
+    nombre(value) {
       if (value?.length >= 2) return true
 
       return 'El nombre debe tener al menos 2 caracteres.'
     },
-    categoria (value) {
+    categoria(value) {
       if (value?.length >= 2) return true
 
       return 'La categoria debe tener al menos 2 caracteres.'
     }
-  },
+  }
 })
 const nombre = useField('nombre')
 const categoria = useField('categoria')
@@ -31,19 +30,17 @@ const nombreActualizar = ref('')
 const medicamentosPorCategoria = ref([])
 const buscarMedCategoria = ref('buscarMedCategoria')
 const listaCategorias = ref([])
-buscarMedCategoria.value =null
+buscarMedCategoria.value = null
 
-const submit = handleSubmit(values => {
-  /*if(medicamentos.has(values.nombre)){
-    alert('Ya existe ese medicamento')
-  }else{
-    medicamentos.set(values.nombre, new Medicamento(values.nombre, values.categoria, values.cantidad, values.precio))
-    console.log('agregado el medicamento')
-    console.log(medicamentos)
-  }*/
-  medicamentos.has(values.nombre) ? alert('Ya existe ese medicamento') : medicamentos.set(values.nombre, new Medicamento(values.nombre, values.categoria, values.cantidad, values.precio))
+const submit = handleSubmit((values) => {
+  medicamentos.has(values.nombre)
+    ? alert('Ya existe ese medicamento')
+    : medicamentos.set(
+        values.nombre,
+        new Medicamento(values.nombre, values.categoria, values.cantidad, values.precio)
+      )
   listaNombres.value.push(values.nombre.toString())
-  if(!buscarCategoria(values.categoria)) listaCategorias.value.push(values.categoria)
+  if (!buscarCategoria(values.categoria)) listaCategorias.value.push(values.categoria)
   console.log(medicamentos)
   console.log(listaNombres.value)
   console.log(listaCategorias.value)
@@ -51,52 +48,50 @@ const submit = handleSubmit(values => {
 
 function buscarCategoria(cat) {
   for (let c of listaCategorias.value) {
-    if(c === cat) return true
+    if (c === cat) return true
   }
-  return false;
+  return false
 }
 
-const aumentarExistencias = () =>{
-  if(!medicamentos.has(nombreActualizar.value)){
+const aumentarExistencias = () => {
+  if (!medicamentos.has(nombreActualizar.value)) {
     alert('No existe ese medicamento')
-  }else {
+  } else {
     medicamentos.get(nombreActualizar.value).aumentarCantidad(existencias.value.value)
     existencias.value.value = 0
     nombreActualizar.value = ' '
   }
 }
-const decrementarExistencias = () =>{
-  if(!medicamentos.has(nombreActualizar.value)){
+const decrementarExistencias = () => {
+  if (!medicamentos.has(nombreActualizar.value)) {
     alert('No existe ese medicamento')
-  }else {
+  } else {
     medicamentos.get(nombreActualizar.value).decrementarCantidad(existencias.value.value)
     existencias.value.value = 0
     nombreActualizar.value = ' '
   }
 }
 
-const buscarPorCategoria = () =>{
+const buscarPorCategoria = () => {
   medicamentosPorCategoria.value.splice(0, medicamentosPorCategoria.value.length)
   let found = false
   let arr = [...medicamentos.values()]
   console.log(buscarMedCategoria.value)
   console.log(arr)
   for (let m of arr) {
-    if(m.categoria === buscarMedCategoria.value){
+    if (m.categoria === buscarMedCategoria.value) {
       medicamentosPorCategoria.value.push(m)
       found = true
     }
   }
-  if(!found)
-    alert('No existen medicamentos en dicha categoria')
+  if (!found) alert('No existen medicamentos en dicha categoria')
 }
-
 </script>
 
 <template>
   <h1 class="mt-10 text-center text-h2 mb-14">Gestión de Inventarios de Farmacia</h1>
-  <v-row>
-    <v-col class="ml-16">
+  <v-row class="ml-3 mr-3 mr-lg-16">
+    <v-col class="ml-lg-16 ml-sm-10">
       <form @submit.prevent="submit" class="w-md-50">
         <h2 class="text-center mb-2">Crear medicamento</h2>
         <v-text-field
@@ -114,7 +109,7 @@ const buscarPorCategoria = () =>{
         ></v-text-field>
 
         <v-number-input
-          class="w-50"
+          class="w-lg-50"
           control-variant="stacked"
           label="Cantidad"
           :max="1000000"
@@ -123,7 +118,7 @@ const buscarPorCategoria = () =>{
         ></v-number-input>
 
         <v-number-input
-          class="w-50"
+          class="w-lg-50"
           control-variant="stacked"
           label="Precio"
           :max="1000000"
@@ -131,19 +126,12 @@ const buscarPorCategoria = () =>{
           v-model="precio.value.value"
         ></v-number-input>
 
-        <v-btn
-          class="me-4 bg-blue font-weight-bold"
-          type="submit"
-        >
-          Crear
-        </v-btn>
+        <v-btn class="me-4 bg-blue font-weight-bold" type="submit"> Crear </v-btn>
 
-        <v-btn @click="handleReset">
-          Limpiar campos
-        </v-btn>
+        <v-btn class="mt-md-3 mt-lg-0" @click="handleReset"> Limpiar campos </v-btn>
       </form>
 
-      <div class="w-md-50 mt-lg-16">
+      <div class="w-md-50 mt-md-16">
         <h2 class="text-center mb-2">Actualizar existencias</h2>
         <v-autocomplete
           class=""
@@ -152,28 +140,24 @@ const buscarPorCategoria = () =>{
           v-model="nombreActualizar"
         ></v-autocomplete>
         <v-number-input
-          class="w-50"
+          class="w-lg-50"
           control-variant="stacked"
           label="Existencias"
           :max="1000000"
           :min="1"
           v-model="existencias.value.value"
         ></v-number-input>
-        <v-btn
-          class="bg-blue font-weight-bold"
-          @click="aumentarExistencias"
-        >
-          Aumentar
-        </v-btn>
-        <v-btn
-          class="bg-blue font-weight-bold ml-10"
-          @click="decrementarExistencias"
-        >
-          Decrementar
-        </v-btn>
+        <v-row class="ml-auto">
+          <v-btn class="bg-blue font-weight-bold" @click="aumentarExistencias"> Aumentar </v-btn>
+          <v-btn
+            class="bg-blue font-weight-bold ml-lg-10 mt-md-3 mt-lg-0"
+            @click="decrementarExistencias"
+            >Decrementar</v-btn
+          >
+        </v-row>
       </div>
     </v-col>
-    <v-col class="mr-16">
+    <v-col class="mr-lg-16">
       <div>
         <h2>Busqueda por categoria</h2>
         <v-autocomplete
@@ -182,27 +166,11 @@ const buscarPorCategoria = () =>{
           :items="listaCategorias"
           v-model="buscarMedCategoria"
         ></v-autocomplete>
-        <v-btn
-          class="bg-blue font-weight-bold"
-          @click="buscarPorCategoria"
-        >
-          Buscar
-        </v-btn>
-        <v-data-table
-          class="mr-15"
-          :items="medicamentosPorCategoria"
-        ></v-data-table>
+        <v-btn class="bg-blue font-weight-bold" @click="buscarPorCategoria"> Buscar </v-btn>
+        <v-data-table class="mr-15" :items="medicamentosPorCategoria"></v-data-table>
       </div>
     </v-col>
-
-
-
-
   </v-row>
-
-
-
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
