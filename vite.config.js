750aa18ff4537fbf31f5bd8574bf4175ec76eb2a
rafaelrdgz <rafaelrdgz2003@gmail.com>
2024-07-11 12:24:15 -0400

    agregado gh-pages y configurado el deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: 'gestion-farmacia-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
