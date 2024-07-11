f9ec57cc377870743fe937ebb990aa9064a29d8f
rafaelrdgz <rafaelrdgz2003@gmail.com>
2024-07-11 14:32:13 -0400

    solucionado error en cantidad y precio

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ const submit = handleSubmit((values) => {
     ? alert('Ya existe ese medicamento')
     : medicamentos.set(
         values.nombre,
-        new Medicamento(values.nombre, values.categoria, values.cantidad, values.precio)
+        new Medicamento(values.nombre, values.categoria, values.precio, values.cantidad)
       )
   listaNombres.value.push(values.nombre.toString())
   if (!buscarCategoria(values.categoria)) listaCategorias.value.push(values.categoria)
